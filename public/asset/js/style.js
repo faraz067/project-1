6dcee44801bb4441f167153e717edc701fccf068
faraz067 <faraz24ti@mahasiswa.pcr.ac.id>
2025-12-15 00:00:10 +0700

    1

--- public/asset/js/style.js
+++
@@ -1,9 +0,0 @@
-/* License ID: DEVOMATE-SRC-20251214-8BIIS7SHXX | unknown@example.com | 2025-12-14 08:37:47 */
-    window.addEventListener('scroll', function() {
-      const navbar = document.querySelector('.navbar');
-      if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-      } else {
-        navbar.classList.remove('scrolled');
-      }
-    });
